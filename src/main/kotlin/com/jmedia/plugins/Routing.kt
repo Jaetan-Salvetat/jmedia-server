0265e166ff41ca42ede256b9ad87d0fd517a4928
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2024-04-04 16:53:34 +0200

    Start to configure SwaggerUI

--- src/main/kotlin/com/jmedia/plugins/Routing.kt
+++ src/main/kotlin/com/jmedia/plugins/Routing.kt
@@ -2,10 +2,15 @@ package com.jmedia.plugins
 
 import com.jmedia.routes.feedback
 import io.ktor.server.application.*
+import io.ktor.server.plugins.swagger.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        feedback()
+        swaggerUI("doc", "openapi/documentation.yaml")
+
+        route("/api/v1") {
+            feedback()
+        }
     }
 }
